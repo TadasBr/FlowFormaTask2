7101d473d89bea176ebce5019ea6acd66cb24074
Tadas <lauokal@gmail.com>
2022-11-02 11:34:30 +0200

    Update TechInventor.js

--- src/js/models/TechInventor.js
+++ src/js/models/TechInventor.js
@@ -4,6 +4,10 @@ export { TechInventor }
  * Tech inventor class
  */
 class TechInventor{
+    name;
+    tech;
+    age;
+    
     constructor(name, tech, age){
         this.name = name;
         this.tech = tech;
